2833c3cb777e292a59085762861a4540519a400e
francescoFH <francescofacca@gmail.com>
2021-02-05 12:47:53 +0000

    Added a database connection class

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -5,8 +5,8 @@ describe Peep do
     it 'returns all peeps' do
       connection = PG.connect(dbname: 'chitter_test')
 
-      connection.exec("INSERT INTO peeps (peep) VALUES('This is my first peep');")
-      connection.exec("INSERT INTO peeps (peep) VALUES('This is my second peep');")
+      Peep.create(peep: 'This is my first peep')
+      Peep.create(peep: 'This is my second peep')
 
       peeps = Peep.all
 
